fea215772f97acdb38b2afbbbad06edbfdefe84a
yunuskarsli <111271279+yunuskarsli@users.noreply.github.com>
2025-02-11 21:13:57 +0300

    Delete src directory

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Orders ,Long> {
-}
