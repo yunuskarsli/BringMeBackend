e5fc20efdcf5fb5c5aeeb5cf3456047ea038989d
yunuskarsli <111271279+yunuskarsli@users.noreply.github.com>
2025-02-14 04:33:40 +0300

    Delete src directory

--- src/main/java/com/example/demo/entity/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.Entity;
-
-
-public enum Role {
-    USER, ADMIN, MODERATOR
-}
--- src/main/java/com/example/demo/repository/TradeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Trade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TradeRepository extends JpaRepository<Trade ,Long> {
-}
